0e7df5496a8343f91cca5c6df8349bda4e1311b1
Shivam Kumar <141820719+shivam-kumar9065@users.noreply.github.com>
2025-07-03 11:38:25 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,15 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json"); // Your actual file path
 
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+const decoded = Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, 'base64').toString('utf-8');
+const credentials = JSON.parse(decoded);
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(credentials)
 });
 
 
